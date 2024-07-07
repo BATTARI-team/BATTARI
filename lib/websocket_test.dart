954475a12789be3fe5a33fe247c6e634a4e40e7d
takuto <tiharu717@gmail.com>
2024-07-07 17:39:53 +0900

    カウントするようにしてみた

--- lib/websocket_test.dart
+++ lib/websocket_test.dart
@@ -12,17 +12,30 @@ class _WebSocketTestState extends State<WebSocketTest> {
   @override
   Widget build(BuildContext context) {
 		final channel = WebSocketChannel.connect(
-			Uri.parse('ws://192.168.11.10:5050/ws'),
+			Uri.parse('ws://192.168.11.10:5050/wstest'),
 		);
 
+		TextEditingController _controller = TextEditingController(text: "hello");
+
 		return StreamBuilder(stream: channel.stream,
+		
 		builder: (context, snapshot) {
+			print(snapshot.hasData ? '${snapshot.data}' : 'No data');
 			return Column(
 			  children: [
 			    Text(snapshot.hasData ? '${snapshot.data}' : 'No data'),
 					TextButton(child: const Text('Send message'),
 					  onPressed: () {
-					    channel.sink.add('Hello!');
+					    channel.sink.add(_controller.text);
+					  },
+					),
+					TextFormField(
+						controller: _controller,
+						decoration: const InputDecoration(labelText: 'Send a message'),
+					),
+					TextButton(child: const Text('Close'),
+					  onPressed: () {
+					    channel.sink.close();
 					  },
 					),
 				],
