b3aff49962675e1ba33bcc2f785a8ab7370426bb
takuto <tiharu717@gmail.com>
2024-07-13 19:42:55 +0900

    通知は出るようになったけど，上から出ない

--- lib/background.dart
+++ lib/background.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class Background extends StatefulWidget {
   const Background({super.key});
@@ -8,49 +9,88 @@ class Background extends StatefulWidget {
   State<Background> createState() => _BackgroundState();
 }
 
+final flutterLocalNotificationPlugin = FlutterLocalNotificationsPlugin();
+const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+
 class _BackgroundState extends State<Background> {
-	int num = 0;
-	bool taskLock = false;
+  int num = 0;
+  bool taskLock = false;
+  int counter_notif_id = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.background,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final androidFlutterLocalNotificationPlugin =
+              flutterLocalNotificationPlugin
+                  .resolvePlatformSpecificImplementation<
+                      AndroidFlutterLocalNotificationsPlugin>();
+          debugPrint("aa");
+          if (androidFlutterLocalNotificationPlugin != null) {
+//            await androidFlutterLocalNotificationPlugin
+//                .requestNotificationsPermission();
+            await flutterLocalNotificationPlugin.show(
+                counter_notif_id,
+                "counter",
+                num.toString(),
+                const NotificationDetails(
+                    android: AndroidNotificationDetails("counter", "counter")));
+          } else {
+            debugPrint("androidFlutterLocalNotificationPlugin is null");
+          }
+        },
       ),
-			child: Text(num.toString(),),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: Text(
+          num.toString(),
+        ),
+      ),
     );
   }
-	@override
-	void initState() {
-		super.initState();
-		_initForegroundTask();
-		FlutterForegroundTask.startService(notificationTitle: "battari", notificationText: "battari text");
-		runTask();
-	}
+
+  @override
+  void initState() {
+    super.initState();
+    _initForegroundTask();
+    flutterLocalNotificationPlugin.initialize(
+        const InitializationSettings(android: initializationSettingsAndroid));
+    FlutterForegroundTask.startService(
+        notificationTitle: "battari", notificationText: "battari text");
+    runTask();
+  }
 
   void _initForegroundTask() {
-		FlutterForegroundTask.init(
-			androidNotificationOptions: AndroidNotificationOptions(channelId: 'foregroundService', channelName: 'battari'), 
-			iosNotificationOptions: const IOSNotificationOptions(showNotification:true, playSound:true), 
-			foregroundTaskOptions: const ForegroundTaskOptions(
-				// タスクを実行する間隔？
-				interval: 5000,
-				isOnceEvent: false,
-				// スマホ起動時に自動実行する？
-				autoRunOnBoot: true,
-			),
-		);
-	}
-	
-	Future<void> runTask() async {
-		taskLock = true;
-		while(taskLock) {
-			await Future.delayed(const Duration(seconds: 1));
-			setState(() {
-				num++;
-				FlutterForegroundTask.updateService(notificationText: "battari text: $num");
-			});
-		}
-		taskLock = false;
-	}
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+          channelId: 'foregroundService',
+          channelName: 'battari',
+          showWhen: true),
+      iosNotificationOptions:
+          const IOSNotificationOptions(showNotification: true, playSound: true),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        // タスクを実行する間隔？
+        interval: 5000,
+        isOnceEvent: false,
+        // スマホ起動時に自動実行する？
+        autoRunOnBoot: true,
+      ),
+    );
+  }
+
+  Future<void> runTask() async {
+    taskLock = true;
+    while (taskLock) {
+      await Future.delayed(const Duration(seconds: 1));
+      setState(() {
+        num++;
+        FlutterForegroundTask.updateService(
+            notificationText: "battari text: $num");
+      });
+    }
+    taskLock = false;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -78,17 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("battari"),
-      ),
-			body: const Background(),
+    return Background();
 //      body: Center(
 //        // Center is a layout widget. It takes a single child and positions it
 //        // in the middle of the parent.
@@ -118,11 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //          ],
 //        ),
 //      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    // This trailing comma makes auto-formatting nicer for build methods.
+//    );
   }
 }
