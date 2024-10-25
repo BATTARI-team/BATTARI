bb1d00e4999ce7b43f9f2292fd9be5b17d590603
takuto1127 <tiharu717@gmail.com>
2024-10-25 14:17:56 +0900

    WebSocketChannelではなく，IOWebSocketChannelを使うことによってhttps headerを定義できる．

--- lib/websocket_test.dart
+++ lib/websocket_test.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-class WebSocketTest extends StatefulWidget{
+class WebSocketTest extends StatefulWidget {
   const WebSocketTest({super.key});
 
   @override
@@ -11,37 +12,37 @@ class WebSocketTest extends StatefulWidget{
 class _WebSocketTestState extends State<WebSocketTest> {
   @override
   Widget build(BuildContext context) {
-		final channel = WebSocketChannel.connect(
-			Uri.parse('ws://192.168.11.10:5050/wstest'),
-		);
+    final channel = IOWebSocketChannel.connect(
+      Uri.parse('ws://169.254.131.195:5050/wstest?token=123456'),
+    );
 
-		TextEditingController _controller = TextEditingController(text: "hello");
+    TextEditingController _controller = TextEditingController(text: "hello");
 
-		return StreamBuilder(stream: channel.stream,
-		
-		builder: (context, snapshot) {
-			print(snapshot.hasData ? '${snapshot.data}' : 'No data');
-			return Column(
-			  children: [
-			    Text(snapshot.hasData ? '${snapshot.data}' : 'No data'),
-					TextButton(child: const Text('Send message'),
-					  onPressed: () {
-					    channel.sink.add(_controller.text);
-					  },
-					),
-					TextFormField(
-						controller: _controller,
-						decoration: const InputDecoration(labelText: 'Send a message'),
-					),
-					TextButton(child: const Text('Close'),
-					  onPressed: () {
-					    channel.sink.close();
-					  },
-					),
-				],
-			);
-		});
+    return StreamBuilder(
+        stream: channel.stream,
+        builder: (context, snapshot) {
+          print(snapshot.hasData ? '${snapshot.data}' : 'No data');
+          return Column(
+            children: [
+              Text(snapshot.hasData ? '${snapshot.data}' : 'No data'),
+              TextButton(
+                child: const Text('Send message'),
+                onPressed: () {
+                  channel.sink.add(_controller.text);
+                },
+              ),
+              TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(labelText: 'Send a message'),
+              ),
+              TextButton(
+                child: const Text('Close'),
+                onPressed: () {
+                  channel.sink.close();
+                },
+              ),
+            ],
+          );
+        });
   }
-
-
 }
