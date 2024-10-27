9f992b6a6db2cfe0e5e45561b104b16287877728
takuto1127 <tiharu717@gmail.com>
2024-10-27 15:47:52 +0900

    [draft] 接続が切れたら，再接続するようにした

--- lib/websocket_test.dart
+++ lib/websocket_test.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
@@ -13,55 +15,127 @@ class WebSocketTest extends StatefulWidget {
 }
 
 class _WebSocketTestState extends State<WebSocketTest> {
+  late IOWebSocketChannel channel;
+  bool isReconnect = false;
+  late Timer timer;
+  late Timer _timer;
+  int i = 0;
+  bool closeStatus = false;
+  reconnectWebSocket() {
+    debugPrint("reconnect作業開始 $isReconnect");
+    if (isReconnect) return;
+    isReconnect = true;
+    timer = Timer.periodic(Duration(seconds: 10), (timer) {
+      // if (timer.tick == 60) {
+      // }
+      print("現在" + timer.tick.toString());
+      try {
+        connectWebscocket();
+        timer.cancel();
+        debugPrint("WebSocket is reconnected");
+      } catch (e) {
+        print("error" + e.toString());
+      }
+    });
+  }
+
+  connectWebscocket() {
+    try {
+      channel = IOWebSocketChannel.connect(Uri.parse('ws://192.168.10.4:5050/ws'), headers: {
+        HttpHeaders.authorizationHeader:
+            // user tokenを入れる
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJCQVRUQVJJLXRlYW0iLCJuYW1laWQiOiJ0YWt1dG8xMTI3IiwibmFtZSI6InRha3V0bzExMjciLCJqdGkiOiJjOWQ5MjIyYy0wNzI1LTQ3MGEtYWY0Yy0zMWIxYmMwNTcyZTgiLCJ1bmlxdWVfbmFtZSI6IjIiLCJleHAiOjE3MzAwNzI1NDV9.DiYTeemV2MetP0pejg3KcbL1HwI8Jwzn03Zh1w2DeVI'
+      });
+      // channel.stream.listen(
+      //   (message) {
+      //     // メッセージを受信したときの処理
+      //     print("Received: $message");
+      //     isReconnect = false;
+      //     i = 0;
+      //   },
+      //   onDone: () {
+      //     // 接続が切断されたときの処理
+      //     debugPrint("WebSocket is closed");
+      //     reconnectWebSocket();
+      //   },
+      //   onError: (error) {
+      //     // エラーが発生したときの処理
+      //     print("Error: $error");
+      //     reconnectWebSocket();
+      //   },
+      // );
+    } catch (e) {
+      print("connectWebscocketでエラー" + e.toString());
+    }
+
+    // channel.sink.done.then((value) async {
+    //   debugPrint("done");
+    // });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    connectWebscocket();
+    () async {
+      _timer = Timer.periodic(Duration(seconds: 10), (timer) {
+        if (i > 3) {
+          debugPrint("WebSocket is closed");
+          channel.sink.close();
+          i = 0;
+        }
+        i++;
+        debugPrint("closeStatus ${channel.closeCode}");
+        if (channel.closeCode != null) {
+          channel.sink.close();
+          reconnectWebSocket();
+          i = 0;
+        }
+      });
+    }();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final channel = IOWebSocketChannel.connect(Uri.parse('ws://192.168.11.14:5050/ws'), headers: {
-      HttpHeaders.authorizationHeader:
-          // user tokenを入れる
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJCQVRUQVJJLXRlYW0iLCJuYW1laWQiOiJ0YWt1dG8xMTI3IiwibmFtZSI6InRha3V0bzExMjciLCJqdGkiOiJjOWQ5MjIyYy0wNzI1LTQ3MGEtYWY0Yy0zMWIxYmMwNTcyZTgiLCJ1bmlxdWVfbmFtZSI6IjIiLCJleHAiOjE3MzAwNzI1NDV9.DiYTeemV2MetP0pejg3KcbL1HwI8Jwzn03Zh1w2DeVI'
-    });
-
     TextEditingController _controller = TextEditingController(text: "hello");
 
-    return StreamBuilder(
-        stream: channel.stream,
-        builder: (context, snapshot) {
-          print(snapshot.hasData ? '${snapshot.data}' : 'No data');
-          return Column(
-            children: [
-              Text(snapshot.hasData ? '${snapshot.data}' : 'No data'),
-              TextButton(
-                child: const Text('Send message'),
-                onPressed: () {
-                  //channel.sink.add(_controller.text);
-                  String myJson = jsonEncode({
-                    'id': 4,
-                    'isWelcome': false,
-                    'incredients': [
-                      {
-                        'type': 'app',
-                        'appData': {
-                          'appName': 'hakondate',
-                          'useTime': 5,
-                        }
-                      },
-                    ]
-                  });
-                  channel.sink.add(myJson);
+    return Column(
+      children: [
+        TextButton(
+          child: const Text('Send message'),
+          onPressed: () {
+            //channel.sink.add(_controller.text);
+            String myJson = jsonEncode({
+              'id': 4,
+              'isWelcome': false,
+              'incredients': [
+                {
+                  'type': 'app',
+                  'appData': {
+                    'appName': 'hakondate',
+                    'useTime': 5,
+                  }
                 },
-              ),
-              TextFormField(
-                controller: _controller,
-                decoration: const InputDecoration(labelText: 'Send a message'),
-              ),
-              TextButton(
-                child: const Text('Close'),
-                onPressed: () {
-                  channel.sink.close();
-                },
-              ),
-            ],
-          );
-        });
+              ]
+            });
+            try {
+              channel.sink.add(myJson);
+            } catch (e) {
+              print("sink addでエラー" + e.toString());
+            }
+          },
+        ),
+        TextFormField(
+          controller: _controller,
+          decoration: const InputDecoration(labelText: 'Send a message'),
+        ),
+        TextButton(
+          child: const Text('Close'),
+          onPressed: () {
+            channel.sink.close();
+          },
+        ),
+      ],
+    );
   }
 }
