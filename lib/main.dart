1920c510ad9093f8d3377c3fe854f691e49bae9d
takuto1127 <tiharu717@gmail.com>
2024-10-31 20:13:32 +0900

    Fix WebSocket connection issue and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:battari/websocket_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-	HttpOverrides.global = MyHttpOverride();
-  runApp(const MyApp());
+Future<void> main() async {
+  HttpOverrides.global = MyHttpOverride();
+  // トークンの初期化
+
+  // コミットしないでね
+
+  await http
+      .post(
+        Uri.parse('http://$IpAddress:5050/User/Login'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'userId': 'takuto1127',
+          'password': 'Mia.kata.1127',
+        }),
+      )
+      .then((value) => Token = jsonDecode(value.body)["token"]);
+  debugPrint("トークンは$Token");
+  runApp(ProviderScope(child: const MyApp()));
 }
 
+const String IpAddress = "10.124.58.206";
+late final String Token;
+
 class MyHttpOverride extends HttpOverrides {
-	@override
-	HttpClient createHttpClient(SecurityContext? context) {
-		return super.createHttpClient(context)
-			..badCertificateCallback = (X509Certificate cert, String host, int port) {
-				return true;
-			};
-	}
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        return true;
+      };
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -100,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: const Text("battari"),
       ),
-			body: const WebSocketTest(),
+      body: const WebSocketTest(),
 //      body: Center(
 //        // Center is a layout widget. It takes a single child and positions it
 //        // in the middle of the parent.
