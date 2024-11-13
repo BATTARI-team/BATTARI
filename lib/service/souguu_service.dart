7dc5eccc407bf63e526edbd5e5c8f1a0254c0c82
takuto1127 <tiharu717@gmail.com>
2024-11-13 12:12:51 +0900

    Refactor SouguuServiceState and SouguuService
    - Refactor SouguuServiceState to a separate file
    - Update import statements for user_form_view_model.dart and user_view_model.dart
    - Update dependencies for json_serializable and json_annotation
    - Add SouguuService class with websocket notification handling

--- lib/service/souguu_service.dart
+++ lib/service/souguu_service.dart
@@ -1,1 +1,31 @@
+import 'dart:convert';
 
+import 'package:battari/model/dto/websocket_souguu_notification.dart';
+import 'package:battari/model/state/souguu_service_state.dart';
+import 'package:battari/service/websocket_service.dart';
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'souguu_service.g.dart';
+
+@Riverpod(keepAlive: true)
+class SouguuService extends _$SouguuService {
+  final Function(String) _dealNotification = (p0) {
+    debugPrint("execute");
+    // ここで受信したデータを処理する
+    if (p0.length > 20) {
+      try {
+        var notif = WebsocketSouguuNotification.fromJson(jsonDecode(p0));
+        debugPrint("受信したデータ: ${notif.souguuReason}");
+      } catch (e) {
+        debugPrint("受信したデータ: ${e.toString()}");
+      }
+    }
+  };
+
+  @override
+  SouguuServiceState build() {
+    ref.watch(websocketServiceProvider);
+    return SouguuServiceState();
+  }
+}
---
+++ lib/service/souguu_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'souguu_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$souguuServiceHash() => r'0176920ed1542afd8b65c2fd251667e6764b4c5d';
+
+/// See also [SouguuService].
+@ProviderFor(SouguuService)
+final souguuServiceProvider =
+    NotifierProvider<SouguuService, SouguuServiceState>.internal(
+  SouguuService.new,
+  name: r'souguuServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$souguuServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SouguuService = Notifier<SouguuServiceState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
